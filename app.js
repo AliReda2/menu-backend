8afbe6e6f7db32564f2b6cc5b57cfbfb13df8221
AliReda2 <alireda2572003@gmail.com>
2025-02-14 15:41:36 +0200

    cors fix

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // Allow requests from the frontend domain
 const allowedOrigins = [
-  "https://menu-frontend-d47kdxq34-alireda2s-projects.vercel.app",
+  "https://menu-frontend-6vfuz69kp-alireda2s-projects.vercel.app",
 ];
 app.use(cors({ origin: allowedOrigins }));
 
